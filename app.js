33254a2902d9d98593fdc62c3c8101aa2d6dcaa8
Apple <siddharth@nisostech.com>
2019-11-08 17:33:51 +0530

    database coonection

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const mongodb = require('mongodb').MongoClient;
 
 const app = express();
 
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost:27017/easy-notes', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongodb.connect('mongodb://127.0.0.1:27017/',).then(() => {
     console.log('connect to the database successfully');
 }).catch(error => {
     console.log('Error in connecting server : ', error);
     process.exit();
-});
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
